724ce068d558b4c3eef0dc6d1d8e2c2cd5617970
choish1919 <choish1919@gmail.com>
2018-11-29 21:10:28 +0900

    Revert "swagger2 removed"
    This reverts commit fc73519

---
+++ src/main/java/com/choish/mjq/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.choish.mjq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.choish.mjq.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("명지퀘스트")
+                .description("명지퀘스트 엔드포인트 API Swagger 문서입니다.")
+                .version("0.0.4")
+                .contact(new Contact("최승훈", "https://github.com/choish1919/", "choish1919@gmail.com"))
+                .build();
+    }
+}
